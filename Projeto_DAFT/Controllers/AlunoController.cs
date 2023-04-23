dab8f82a7866d588b876cc287423cfe133341950
AlexandreFV <alexandre.santos141@fatec.sp.gov.br>
2023-04-23 13:48:53 -0300

    Tela de escolha do tipo de Cadastro Finalizada.

---
+++ Projeto_DAFT/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_DAFT.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
