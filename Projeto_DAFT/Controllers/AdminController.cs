11c46a1fc24c59106cabd40d4323b50a7a4ab9d2
AlexandreFV <alexandre.santos141@fatec.sp.gov.br>
2023-04-29 18:00:26 -0300

    Criação do Controller e View Do Admin, Admin adicionado a tela de escolha de Cadastro. Tela de Cadastro (completa)

---
+++ Projeto_DAFT/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_DAFT.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
