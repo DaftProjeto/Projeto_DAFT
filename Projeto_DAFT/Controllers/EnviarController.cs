babc871ce606bb5ed4a0172516c17491d170b4d0
ArthurAmericoDurante <projeto.daft@gmail.com>
2023-05-04 17:19:24 -0300

    Inicio da criação da tela de envio do Anteprojeto (Imcompleto).

---
+++ Projeto_DAFT/Controllers/EnviarController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_DAFT.Controllers
+{
+    public class EnviarController : Controller
+    {
+        public IActionResult EnviarAnteprojeto()
+        {
+            return View();
+        }
+
+        public IActionResult EnviarTCC()
+        {
+            return View();
+        }
+    }
+}
