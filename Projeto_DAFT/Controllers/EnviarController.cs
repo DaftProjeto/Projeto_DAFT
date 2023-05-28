493490bb4991b3330b829a36a0df5141df9bc3c3
ViniciusChanes <vini.chanes@gmail.com>
2023-05-28 20:39:29 -0300

    Começo do INSERT de projetos, Alterações do ProjetoEntidade por faltar valores, Copiei a tela de EnviarAnteprojeto da brench master

--- Projeto_DAFT/Controllers/EnviarController.cs
+++ Projeto_DAFT/Controllers/EnviarController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Projeto_DAFT.Entidades;
 
 namespace Projeto_DAFT.Controllers
 {
     public class EnviarController : Controller
     {
+        private Contexto contexto;
+
+        public EnviarController(Contexto db)
+        {
+            contexto = db;
+        }
         public IActionResult EnviarAnteprojeto()
         {
             return View();
@@ -17,6 +24,15 @@ namespace Projeto_DAFT.Controllers
         public IActionResult EnviarTCCPage2()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult EnvioProjeto(ProjetoEntidade proj)
+        {
+            contexto.Projeto.Add(proj);
+            contexto.SaveChanges();
+            return RedirectToAction("/Home/Gerenciador_Ativades_Curriculares");
         }
     }
 }
--- Projeto_DAFT/Entidades/ProjetoEntidade.cs
+++ Projeto_DAFT/Entidades/ProjetoEntidade.cs
@@ -6,6 +6,7 @@
         public string Titulo { get; set; }
         public string Tipo { get; set; }
         public string Caminho { get; set; }
+        public string Turno { get; set; }
 
         public int AlunoId{ get; set; }
         public AlunoEntidade Aluno { get; set; }
